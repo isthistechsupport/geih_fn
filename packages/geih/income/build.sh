87621e9a65ceb1cfd87abcdcb7fdaaadc9af74a7
isthistechsupport <32936232+isthistechsupport@users.noreply.github.com>
2023-06-03 19:03:22 -0500

    trying again with python 3.9

--- packages/geih/income/build.sh
+++ packages/geih/income/build.sh
@@ -5,7 +5,8 @@ set -e
 virtualenv --without-pip virtualenv
 
 # Uncomment if you're using the Python 3.9 runtime
-# pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
+pip install --platform=manylinux1_x86_64 --only-binary=:all: psycopg2-binary --target virtualenv/lib/python3.9/site-packages
+pip install SQLAlchemy --target virtualenv/lib/python3.9/site-packages
 
 # Uncomment if you're using the Python 3.11 runtime
-pip install -r requirements.txt --target virtualenv/lib/python3.11/site-packages
+# pip install -r requirements.txt --target virtualenv/lib/python3.11/site-packages
