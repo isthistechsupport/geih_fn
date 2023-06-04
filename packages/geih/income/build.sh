24dd1d469342fe70b397cc4d92ad3111c170e8a8
isthistechsupport <32936232+isthistechsupport@users.noreply.github.com>
2023-06-03 21:35:33 -0500

    more reverts

--- packages/geih/income/build.sh
+++ packages/geih/income/build.sh
@@ -5,8 +5,8 @@ set -e
 virtualenv --without-pip virtualenv
 
 # Uncomment if you're using the Python 3.9 runtime
-pip install psycopg2-binary --target virtualenv/lib/python3.11/site-packages --platform=manylinux1_x86_64
-pip install SQLAlchemy --target virtualenv/lib/python3.11/site-packages
+# pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
 
 # Uncomment if you're using the Python 3.11 runtime
 # pip install -r requirements.txt --target virtualenv/lib/python3.11/site-packages
+pip install -r requirements.txt --target virtualenv/lib/python3.11/site-packages
