41f7a61ab85317bea888fd6c72827c286cbe95b2
isthistechsupport <32936232+isthistechsupport@users.noreply.github.com>
2023-06-03 22:33:37 -0500

    changed invocations

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serverless function for the salary o meter
+
+IMPORTANT: must be deployed with `doctl sls deploy . --remote-build`
--- packages/geih/income/__main__.py
+++ packages/geih/income/__main__.py
@@ -18,13 +18,7 @@ def get_tables(metadata: MetaData, engine: Engine):
         "fact_income_permille_dept_zone", metadata, autoload_with=engine
     )
     divipola_table = Table("seed_divipola", metadata, autoload_with=engine)
-    return (
-        income_table,
-        income_dept_table,
-        income_dept_zone_table,
-        divipola_table,
-        engine,
-    )
+    return (income_table, income_dept_table, income_dept_zone_table, divipola_table)
 
 
 def get_income_quantiles(
@@ -74,18 +68,19 @@ def get_income_quantiles(
             select(divipola_table.c.name).where(divipola_table.c.code == department)
         )[0].name
     return (
+        str(dept_str),
         int(quantile_below),
         int(quantile_above),
         int(dept_quantile_below),
         int(dept_quantile_above),
         int(dept_zone_quantile_below),
         int(dept_zone_quantile_above),
-        str(dept_str),
     )
 
 
 def prettify_results(
     income: int,
+    dept_str: str,
     zone: int,
     quantile_below: int,
     quantile_above: int,
@@ -93,7 +88,6 @@ def prettify_results(
     dept_quantile_above: int,
     dept_zone_quantile_below: int,
     dept_zone_quantile_above: int,
-    dept_str: str,
 ):
     result = {}
     single_quantile = False
@@ -154,20 +148,22 @@ def prettify_results(
 def main(args):
     income = args.get("income", -1)
     department = args.get("department", "unknown")
-    zone = args.get("zone", "unknown")
-    if int(income) < 0 or department == "unknown" or zone == "unknown":
+    zone = args.get("zone", -1)
+    if int(income) < 0 or department == "unknown" or int(zone) == -1:
         return {"error": "Please provide income, department, and zone."}
     # get connection parameters from environment variables
-    dbprotocol = os.environ['DBPROTOCOL']
-    dbdriver = os.environ['DBDRIVER']
-    dbuser = os.environ['DBUSER']
-    dbpassword = os.environ['DBPASSWORD']
-    dbhost = os.environ['DBHOST']
-    dbport = os.environ['DBPORT']
-    dbname = os.environ['DBNAME']
-    dbsslmode = os.environ['DBSSLMODE']
-    connstring = f"{dbprotocol}+{dbdriver}://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}"
-    engine = create_engine(connstring, connect_args={'sslmode': dbsslmode})
+    dbprotocol = os.environ["DBPROTOCOL"]
+    dbdriver = os.environ["DBDRIVER"]
+    dbuser = os.environ["DBUSER"]
+    dbpassword = os.environ["DBPASSWORD"]
+    dbhost = os.environ["DBHOST"]
+    dbport = os.environ["DBPORT"]
+    dbname = os.environ["DBNAME"]
+    dbsslmode = os.environ["DBSSLMODE"]
+    connstring = (
+        f"{dbprotocol}+{dbdriver}://{dbuser}:{dbpassword}@{dbhost}:{dbport}/{dbname}"
+    )
+    engine = create_engine(connstring, connect_args={"sslmode": dbsslmode})
     metadata = MetaData(schema="public")
     metadata.reflect(
         bind=engine,
@@ -178,16 +174,39 @@ def main(args):
             "seed_divipola",
         ],
     )
+    (
+        income_table,
+        income_dept_table,
+        income_dept_zone_table,
+        divipola_table,
+    ) = get_tables(metadata=metadata, engine=engine)
+    (
+        dept_str,
+        quantile_below,
+        quantile_above,
+        dept_quantile_below,
+        dept_quantile_above,
+        dept_zone_quantile_below,
+        dept_zone_quantile_above,
+    ) = get_income_quantiles(
+        income=int(income),
+        department=department,
+        zone=int(zone),
+        income_table=income_table,
+        income_dept_table=income_dept_table,
+        income_dept_zone_table=income_dept_zone_table,
+        divipola_table=divipola_table,
+        engine=engine,
+    )
     result = prettify_results(
         income=int(income),
+        dept_str=dept_str,
         zone=int(zone),
-        *get_income_quantiles(
-            income=int(income),
-            department=department,
-            zone=int(zone),
-            *get_tables(metadata, engine),
-        ),
+        quantile_below=quantile_below,
+        quantile_above=quantile_above,
+        dept_quantile_below=dept_quantile_below,
+        dept_quantile_above=dept_quantile_above,
+        dept_zone_quantile_below=dept_zone_quantile_below,
+        dept_zone_quantile_above=dept_zone_quantile_above,
     )
-    return {
-		"body": result
-	}
+    return {"body": result}
