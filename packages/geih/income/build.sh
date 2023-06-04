696b7c7cf34bb2bc484fdea1e8b91501278d2118
isthistechsupport <32936232+isthistechsupport@users.noreply.github.com>
2023-06-03 19:11:10 -0500

    changing args

--- packages/geih/income/build.sh
+++ packages/geih/income/build.sh
@@ -5,7 +5,7 @@ set -e
 virtualenv --without-pip virtualenv
 
 # Uncomment if you're using the Python 3.9 runtime
-pip install --platform=manylinux1_x86_64 --only-binary=:all: psycopg2-binary --target virtualenv/lib/python3.11/site-packages
+pip install psycopg2-binary --target virtualenv/lib/python3.11/site-packages --platform=manylinux1_x86_64
 pip install SQLAlchemy --target virtualenv/lib/python3.11/site-packages
 
 # Uncomment if you're using the Python 3.11 runtime
